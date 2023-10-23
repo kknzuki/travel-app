59adf7aaa99b78c4ea5869e246e54cfc812f7647
kknzuki <kennzuki@gmail.com>
2023-10-23 15:20:27 +0300

    awards component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import Hero from '@/components/Hero';
 import Grid from '@/components/Grid';
 import Image from 'next/image';
 import Features from '@/components/Features';
+import Award from '@/components/Award';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Grid />
-      <Features/>
+      <Features />
+      <Award />
     </main>
   );
 }
---
+++ src/components/Award.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import React from 'react';
+
+export default function Award() {
+  return (
+    <div>
+      <section className='flex flex-col md:flex-row justify-evenly gap-2 items-center align-center border-t-4 p-4 mx-auto'>
+        <article className='p-3 text-center '>
+          <h1 className='text-2xl md:text-4xl font-bold p-2 uppercase'>Travelers Choice Best of the Best</h1>
+          <p className='text-center text-wrap'>Our top 5% of places, stays, eats, and experiences—decided by you.</p>
+          <button className='bg-green-600 text-white rounded-xl p-2 text-center my-4'>See the Best</button>
+        </article>
+       <Image src='/pic (1).png' height={400} width={400} alt='pic' className='p-2 rounded-[65%] rotate-120 text-center'/>
+      </section>
+    </div>
+  );
+}
